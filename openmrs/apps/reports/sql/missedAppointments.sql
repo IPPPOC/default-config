cf65001d63a830288a93d4950f8ba4878db4bbd4
shivakumar502 <shivakumarakula502@gmail.com>
2017-11-29 11:04:11 +0530

    Shiva Kumar | Adding date range for list of appointments report

--- openmrs/apps/reports/sql/missedAppointments.sql
+++ openmrs/apps/reports/sql/missedAppointments.sql
@@ -1,24 +1,24 @@
-SELECT
-	pi.identifier,
-     CONCAT(pname.given_name, ' ', pname.family_name)  AS Patient Name,
-     app_service.name                                                                                AS 'Appointment Service',
-     app_service_type.name                                                                           AS 'Appointment Service Type',
-     DATE_FORMAT(start_date_time, "%d/%m/%Y")                                                        AS 'Date of Appointment',
-     CONCAT(DATE_FORMAT(start_date_time, "%l:%i %p"), " - ", DATE_FORMAT(end_date_time, "%l:%i %p")) AS 'Slot',
-     CONCAT(pn.given_name, ' ', pn.family_name)                                                      AS 'Provider',
-     pa.status                                                                                       AS 'Status',
-     pa.comments																					 AS 'Notes'
-FROM
-   patient_appointment pa
-   JOIN person p ON p.person_id = pa.patient_id AND pa.voided IS FALSE
-   JOIN person_name pname on pname.person_id = p.person_id
-   JOIN patient_identifier pi on pi.patient_id = p.person_id
-   JOIN appointment_service app_service
-     ON app_service.appointment_service_id = pa.appointment_service_id AND app_service.voided IS FALSE
-   LEFT JOIN provider prov ON prov.provider_id = pa.provider_id AND prov.retired IS FALSE
-   LEFT JOIN person_name pn ON pn.person_id = prov.person_id AND pn.voided IS FALSE
-   LEFT JOIN appointment_service_type app_service_type
-     ON app_service_type.appointment_service_type_id = pa.appointment_service_type_id
- WHERE  start_date_time < CURDATE() AND (app_service_type.voided IS FALSE OR app_service_type.voided IS NULL) AND pa.status != "Completed"
- ORDER BY start_date_time DESC;
+     SELECT
+         pi.identifier AS 'Patient ID',
+          CONCAT(pname.given_name, ' ', pname.family_name)  AS 'Patient Name',
+          app_service.name                                                                                AS 'Appointment Service',
+          app_service_type.name                                                                           AS 'Appointment Service Type',
+          DATE_FORMAT(start_date_time, "%d/%m/%Y")                                                        AS 'Date of Appointment',
+          CONCAT(DATE_FORMAT(start_date_time, "%l:%i %p"), " - ", DATE_FORMAT(end_date_time, "%l:%i %p")) AS 'Slot',
+          CONCAT(pn.given_name, ' ', pn.family_name)                                                      AS 'Provider',
+          pa.status                                                                                       AS 'Status',
+          pa.comments																					 AS 'Notes'
+     FROM
+        patient_appointment pa
+        JOIN person p ON p.person_id = pa.patient_id AND pa.voided IS FALSE
+        JOIN person_name pname on pname.person_id = p.person_id
+        JOIN patient_identifier pi on pi.patient_id = p.person_id
+        JOIN appointment_service app_service
+          ON app_service.appointment_service_id = pa.appointment_service_id AND app_service.voided IS FALSE
+        LEFT JOIN provider prov ON prov.provider_id = pa.provider_id AND prov.retired IS FALSE
+        LEFT JOIN person_name pn ON pn.person_id = prov.person_id AND pn.voided IS FALSE
+        LEFT JOIN appointment_service_type app_service_type
+          ON app_service_type.appointment_service_type_id = pa.appointment_service_type_id
+      WHERE   DATE(start_date_time) BETWEEN CAST('#startDate#' AS DATE) AND CAST('#endDate#' AS DATE)  AND (app_service_type.voided IS FALSE OR app_service_type.voided IS NULL) AND pa.status != "Completed"
+      ORDER BY start_date_time DESC
 
